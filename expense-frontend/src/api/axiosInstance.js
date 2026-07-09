0140943a0712dc5016cc2ee223ad8eca4ce8c370
igurleenkaur25 <igurleenkaur25@gmail.com>
2026-07-09 15:13:00 +0530

    Point frontend to deployed backend

--- expense-frontend/src/api/axiosInstance.js
+++ expense-frontend/src/api/axiosInstance.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expense-tracker-nbex.onrender.com",
 });
 
 API.interceptors.request.use(
